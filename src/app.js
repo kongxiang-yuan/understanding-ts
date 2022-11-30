39ea6a5e8ad60eca33d72714e701498204da4e78
kongxiang-yuan <kongxiang_yuan@163.com>
2022-11-30 10:32:01 +0800

    Array 类型

--- src/app.js
+++ src/app.js
@@ -1,9 +1,21 @@
 /**
  * @declaration Core Types
- * object
+ * object Arrays
  */
+// const person: {
+//   name: string
+//   age: number
+// } = {
 var person = {
     name: 'kongxiangyuan',
-    age: 21
+    age: 21,
+    hobbies: ['Sports', 'Cooking']
 };
-console.log(person);
+var favoriteActivites;
+favoriteActivites = ['Sports'];
+console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobble = _a[_i];
+    console.log(hobble.toUpperCase());
+    // console.log(hobble.map); // 类型“string”上不存在属性“map”。ts(2339)
+}
--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,25 @@
 /**
  * @declaration Core Types
- * object 
+ * object Arrays
  */
 
-const person: {
-  name: string
-  age: number,
-} = {
-  name: 'kongxiangyuan',
-  age: 21,
-}
-console.log(person)
+// const person: {
+//   name: string
+//   age: number
+// } = {
+  const person = {
+    name: 'kongxiangyuan',
+    age: 21,
+    hobbies: ['Sports', 'Cooking'],
+  }
+  
+  let favoriteActivites: string[]
+  favoriteActivites = ['Sports']
+  console.log(person.name)
+  
+  
+  for(const hobble of person.hobbies){
+    console.log(hobble.toUpperCase());
+    // console.log(hobble.map); // 类型“string”上不存在属性“map”。ts(2339)
+  
+  }
