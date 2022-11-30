60aa7e2c240bd16c7d472048e886a5eeb3ca1278
kongxiang-yuan <kongxiang_yuan@163.com>
2022-11-30 10:31:05 +0800

    object 类型

--- src/app.js
+++ src/app.js
@@ -1,21 +1,9 @@
 /**
  * @declaration Core Types
- * object Arrays
+ * object
  */
-// const person: {
-//   name: string
-//   age: number
-// } = {
 var person = {
     name: 'kongxiangyuan',
-    age: 21,
-    hobbies: ['Sports', 'Cooking']
+    age: 21
 };
-var favoriteActivites;
-favoriteActivites = ['Sports'];
-console.log(person.name);
-for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
-    var hobble = _a[_i];
-    console.log(hobble.toUpperCase());
-    // console.log(hobble.map); // 类型“string”上不存在属性“map”。ts(2339)
-}
+console.log(person);
---
+++ src/app.ts
@@ -0,0 +1,13 @@
+/**
+ * @declaration Core Types
+ * object 
+ */
+
+const person: {
+  name: string
+  age: number,
+} = {
+  name: 'kongxiangyuan',
+  age: 21,
+}
+console.log(person)
