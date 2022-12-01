b10599d9e0e3ac2554e8f5c55de8b01fa9bc84f6
kongxiang-yuan <kongxiang_yuan@163.com>
2022-12-01 12:27:31 +0800

    unknonw

--- src/app.js
+++ src/app.js
@@ -1,23 +1,11 @@
 /**
  * @declaration Core Types
- * Function & callbacks
+ * Unknonw
  */
-function add(n1, n2) {
-    return n1 + n2;
+var userInput;
+var userName;
+userInput = 1;
+userInput = '1';
+if (typeof userInput === 'string') {
+    userName = userInput;
 }
-function addHanHandle(n1, n2, cb) {
-    var reslut = n1 + n2;
-    cb(reslut);
-}
-function printReslut(num) {
-    console.log('Reslut: ', num);
-}
-printReslut(add(20, 1));
-var combineValues;
-combineValues = add;
-// combineValues = printReslut
-// combineValues = 5 //不能将类型“number”分配给类型“Function”。ts
-console.log(combineValues(8, 8));
-addHanHandle(10, 20, function (reslut) {
-    console.log(reslut);
-});
---
+++ src/app.ts
@@ -0,0 +1,13 @@
+/**
+ * @declaration Core Types
+ * Unknonw
+ */
+
+let userInput: unknown
+let userName: string
+userInput = 1
+userInput = '1'
+
+if (typeof userInput === 'string') {
+  userName = userInput
+}
