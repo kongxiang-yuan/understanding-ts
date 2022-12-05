f87ec1f18a3349efdbe6e87a80bd34cff9963007
kongxiang-yuan <kongxiang_yuan@163.com>
2022-12-05 17:27:22 +0800

    工具类型

---
+++ src/app.ts
@@ -0,0 +1,29 @@
+/**
+ * @declaration 工具类型
+ */
+
+// Required<Type> 必须
+interface UsetInfo {
+  name: string
+  gmail: string
+  date: Date
+}
+
+function submit(param: Required<UsetInfo>) {
+  return param as UsetInfo
+}
+console.log(submit({ name: 'kongxianyuan', gmail: '', date: new Date() }))
+// Partial<Type> 可选
+interface UsetInfo {
+  name: string
+  gmail: string
+  date: Date
+}
+
+function EditUserInfo(param: Partial<UsetInfo>) {
+  return param as UsetInfo
+}
+console.log(EditUserInfo({ name: 'kongxianyuan' }))
+
+// Readonly<Type> 只读
+const names: Readonly<string[]> = ['kongxiangyuan']
